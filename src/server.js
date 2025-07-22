0c548d120c22ab5ccd1b198722bf9d15d2f3626b
redouane13x <redredouane896@gmail.com>
2025-07-22 15:45:27 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-// Express server placeholder
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Services from "./pages/Services";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+    </Router>
+  );
+}// Express server placeholder
